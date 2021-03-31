888f0b068c0f216163803443ab44bd4abbd6e91c
pilgr1m <vanjavolokha@gmail.com>
2021-03-31 18:10:08 +0300

    add first styles

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,5 +1,47 @@
+import Grid from "@material-ui/core/Grid"
+import Container from "@material-ui/core/Container"
+import { makeStyles, createStyles } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import { useContacts } from "./useContacts"
+
+const useStyles = makeStyles((theme) =>
+	createStyles({
+		root: {
+			marginTop: theme.spacing(4),
+		},
+		headContainer: {
+			marginBottom: theme.spacing(3),
+		}
+	}))
+
 
 export const Contacts = () => {
-    return <div> Contacts </div>
+	const classes = useStyles();
+
+	const contacts = useContacts()
+
+	if (contacts.isLoading) {
+		return <div> ...loading</div>
+	}
+	if (contacts.isError) {
+		return <div> ...error</div>
+	}
+
+	return (
+		<Container className={classes.root}>
+			<Grid container>
+				<Grid item xs={12} className={classes.headContainer}>
+					<Typography variant="h3" component="h1">
+						Contacts:
+				</Typography>
+				</Grid>
+				<Grid item xs={12}>
+					<> {contacts.data[0].name.first} </>
+				</Grid>
+			</Grid>
+		</Container>
+	)
+
+
 }
 
