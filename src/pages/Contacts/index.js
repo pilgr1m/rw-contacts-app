c094bf7d5205a62f76762f155cf16a6a7ac45fd9
pilgr1m <vanjavolokha@gmail.com>
2021-04-05 12:34:10 +0300

    simply test

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -44,7 +44,7 @@ export const Contacts = () => {
 				<Grid item xs={12}>
 					{(() => {
 						if (contacts.isLoading) {
-							return <CircularProgress />
+							return <CircularProgress data-testid="contacts-loader" />
 						}
 						if (contacts.isError) {
 							return <div> ...error</div>
